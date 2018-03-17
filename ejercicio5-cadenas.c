ac281e763ad467ea3d410c2742a4d0db9e60a1d1
mariabosaba <36692966+mariabosaba@users.noreply.github.com>
2018-03-17 19:42:53 +0100

    Update ejercicio5-cadenas.c

--- ejercicio5-cadenas.c
+++ ejercicio5-cadenas.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #define N 500
-/*Ejercicio	5: Realice	un	programa	que	cambia	los	espacios	en	blanco	por	“	_”	en	la	frase: “Sería	
-posible	describir	todo	científicamente,	pero	no	tendría	ningún	sentido;	carecería	de	significado	
-el	 que	 usted	 describiera	 a	 la	 sinfonía	 de	 Beethoven	 como	 una	 variación	 de	 la	 presión	 de	 la	
-onda	auditiva”.	.*/
+/*Ejercicio	5: Realice	un	programa	que	cambia	los	espacios	en	blanco	por	â€œ	_â€	en	la	frase: â€œSerÃ­a	
+posible	describir	todo	cientÃ­ficamente,	pero	no	tendrÃ­a	ningÃºn	sentido;	carecerÃ­a	de	significado	
+el	 que	 usted	 describiera	 a	 la	 sinfonÃ­a	 de	 Beethoven	 como	 una	 variaciÃ³n	 de	 la	 presiÃ³n	 de	 la	
+onda	auditivaâ€.	
+.*/
 void main() {
 	char frase[N] = ("Seria posible describir todo cientificamente pero no tendria ningun sentido careceria de significado el que usted describiera a la sinfonia de Beethoven como una variacion de la presion de la onda auditiva");
 	int i;
 	i = 0;
-	while (frase[i] != '\0') {//Bucle hasta la ultima tera de la frase
-		if (frase[i] == ' ') {//si el caracter espacio se encuentra en la posición i,lo                             sustituye por '_'
-			frase[i] = '_';
+	while (frase[i] != '\0') {//Bucle hasta la ultima letra de la frase
+		if (frase[i] == ' ') {//si el caracter espacio se encuentra en la posiciÃ³n i,lo sustituye por '_'
+		   
+	            frase[i] = '_';
 		}
 		i++;
 	}
